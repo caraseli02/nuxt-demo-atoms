f60e1f121c556d69e460febacdb3dcce22e4361b
vlad <caraseli02@gmail.com>
2023-08-23 18:25:21 +0200

    feat: add Tabs and ResponsiveCard component

---
+++ components/ResponsiveCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="responsive-card">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.responsive-card {
+  background-color: #ffffff;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  padding: 20px;
+  width: 100%;
+  margin: 0 auto;
+  box-sizing: border-box;
+}
+</style>
---
+++ components/ThemesList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <h4 class="themes-title">Title</h4>
+    <section class="theme-btn">
+      <Button v-for="theme in themes" :key="theme" :btnType="theme">
+        {{ theme }}
+      </Button>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+const themes = ['ambiente', 'economia', 'mundo', 'non-profit', 'politica', 'societa', 'welfare']
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  width: 90rem;
+  padding: 4rem 2.5rem;
+  align-items: flex-start;
+  gap: 2rem;
+}
+
+.theme-btns {
+  display: flex;
+  align-items: flex-start;
+  gap: 1rem;
+}
+
+.themes-title {
+  flex: 1 0 0;
+  color: #010101;
+  font-family: Anybody;
+  font-size: 3.1875rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 2.875rem;
+}
+</style>
