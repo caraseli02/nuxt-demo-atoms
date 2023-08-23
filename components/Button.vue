7f048ca774a10e27f9ea62805d725faf2374fa2c
vlad <caraseli02@gmail.com>
2023-08-23 15:13:50 +0200

    feat: finish button component

--- components/Button.vue
+++ components/Button.vue
@@ -1,55 +1,112 @@
 <template>
-  <button
-    :class="buttonClass"
-    :disabled="disabled"
-    @click="handleClick"
-    :aria-label="ariaLabel"
-  >
-    <slot></slot>
+  <button :class="buttonClass" :disabled="disabled" @click="handleClick" :aria-label="ariaLabel || 'Button'">
+    <slot name="icon" />
+    <slot>{{ label }}</slot>
   </button>
 </template>
 
-<script>
-export default {
-  name: 'atomsButton',
-  props: {
-    type: String,
-    disabled: Boolean,
-    ariaLabel: String,
-  },
-  computed: {
-    buttonClass() {
-      return [
-        'atoms-button',
-        {
-          'atoms-button-primary': this.type === 'primary',
-          'atoms-button-secondary': this.type === 'secondary',
-          'atoms-button-disabled': this.disabled,
-        },
-      ];
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+type ButtonType = 'ambiente' | 'economia' | 'mundo' | 'non-profit' | 'politica' | 'societa' | 'welfare'
+type ButtonSize = 'S' | 'M' | 'L' | 'XL'
+const { label, btnType, disabled = false, ariaLabel, btnSize = 'L' } = defineProps<{
+  btnType?: ButtonType,
+  disabled: boolean,
+  ariaLabel: string,
+  btnSize: ButtonSize,
+  label: string
+}>()
+const emit = defineEmits(['click'])
+
+const isActive = ref(false)
+const buttonClass = computed(() => {
+  return [
+    'atoms-button',
+    {
+      'atoms-button-disabled': disabled,
     },
-  },
-  methods: {
-    handleClick() {
-      if (!this.disabled) {
-        this.$emit('click');
-      }
-    },
-  },
-};
+    `atoms-button-${btnSize}`,
+    isActive.value ? btnType : ''
+  ];
+})
+const handleClick = () => {
+  // if (!disabled) {
+  //   return emit('click');
+  // }
+  if(btnType){
+    isActive.value = !isActive.value;
+  }
+}
 </script>
 
 <style scoped>
 .atoms-button {
   display: inline-flex;
-  padding: 0.75rem 1rem;
-  align-items: center;
   gap: 0.25rem;
   border-radius: 0.25rem;
   border: 1px solid #000;
   background: #FFF;
   box-shadow: -2px 2px 0px 0px #000;
+  color: #000;
+  font-size: 0.625rem;
+  font-style: normal;
+  font-weight: 700;
+  text-transform: uppercase;
+  transition: all 0.5s linear;
 }
 
-/* ... rest of the styles ... */
+.atoms-button-S {
+  height: 1.25rem;
+  padding: 0.25rem 0.75rem;
+  align-items: flex-start;
+  flex-shrink: 0;
+}
+
+.atoms-button-M {
+  height: 1.75rem;
+  padding: 0.5rem 0.75rem;
+  align-items: center;
+  flex-shrink: 0;
+}
+
+.atoms-button-L {
+  padding: 0.75rem 1rem;
+  align-items: center;
+}
+
+.atoms-button-XL {
+  display: inline-flex;
+  padding: 0.75rem 1.5rem;
+  align-items: center;
+  font-size: 1.125rem;
+}
+
+.ambiente {
+  background-color: #E2F1E8;
+}
+
+.economia {
+  background-color: #E2E8F1;
+}
+
+.mundo {
+  background-color: #F1DADA;
+}
+
+.non-profit {
+  background-color: #F0E2F1;
+}
+
+.politica {
+  background-color: #F1F1E2;
+}
+
+.societa {
+  background-color: #E2F0F1;
+}
+
+.welfare {
+  background-color: #F5E6E0;
+}
 </style>
