24b39256f6bf22a97671ac4f1fdc9f2a9047bfc7
vlad <caraseli02@gmail.com>
2023-08-25 14:18:49 +0200

    feat: add hero articles

--- components/ResponsiveCard.vue
+++ components/ResponsiveCard.vue
@@ -1,21 +1,79 @@
 <template>
   <div class="responsive-card">
     <slot></slot>
+    <section class="content">
+      <AppButton label="Loremmm" btn-size="S" />
+      <div class="content-bottom">
+        <h2 class="card-title">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h2>
+        <AppAvatar class="avatar-font" title="Lorem isposn" subtitle="13 Luglio 2022" />
+      </div>
+    </section>
   </div>
 </template>
 
 <script setup lang="ts">
-
+const props = defineProps<{
+  imgUrl?: string,
+}>()
+const imgUrl = computed(() => {
+  return props.imgUrl || "url('https://picsum.photos/seed/{home}/1200/1300')";
+})
 </script>
 
 <style scoped>
-.responsive-card {
-  background-color: #ffffff;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-  border-radius: 8px;
-  padding: 20px;
+@import url('https://fonts.googleapis.com/css2?family=Anybody:wght@400;700&display=swap');
+
+.content {
+  display: flex;
   width: 100%;
-  margin: 0 auto;
-  box-sizing: border-box;
+  height: 100%;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  gap: 1.5rem;
+}
+
+.content-bottom{
+  justify-self: end;
+}
+
+.responsive-card {
+  background-image: v-bind(imgUrl);
+  display: flex;
+  width: 100%;
+  height: 100%;
+  padding: 2.75rem 2.5rem;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  gap: 1.5rem;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.card-title {
+  color: #FFF;
+  font-family: Anybody;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 3.375rem;
+  width: max(70vw, 24.375remm);
+}
+
+@media (min-width: 1250px) {
+  .card-title {
+  font-size: 2.5rem;
+  width: max(60vw, 51.4375rem);
+
+  }
+
+  .responsive-card {
+    padding: 1.5rem 1rem;
+  }
+}
+
+.avatar-font {
+  font-family: Anybody;
 }
 </style>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <AppTabs @tabChange="selectedTab = $event" class="py-6 px-10 hidden lg:block" :tabs="['Tutti i temi', ...tabs]" />
+    <section
+      class="py-4 px-6 lg:py-6 lg:px-10 flex gap-3 lg:gap-0 flex-col md:flex-row w-screen justify-between items-center bg-[#F4F4F4] border-b border-t border-black">
+      <h4 class="font-bold leading-[2.875rem] w-full text-start text-lg lg:text-5xl capitalize">
+        {{ selectedTab }}
+      </h4>
+      <div class="flex gap-2 lg:gap-4 flex-wrap lg:flex-nowrap shrink-0">
+        <AppButton v-for="item in tabs" :label="item" :btnType="item" />
+      </div>
+    </section>
+    <ResponsiveCard class="min-h-[32rem]" />
+    <section class="flex flex-col lg:grid grid-rows-2 grid-cols-2 h-[52rem]">
+      <ResponsiveCard imgUrl="url('https://picsum.photos/seed/{allarmi}/1200/1300')" class="row-span-2" />
+      <ResponsiveCard imgUrl="url('https://picsum.photos/seed/{esperimenti}/1200/1300')" />
+      <ResponsiveCard imgUrl="url('https://picsum.photos/seed/{ucraina}/1200/1300')" />
+    </section>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+const tabs = ['ambiente', 'economia', 'mundo', 'non-profit', 'politica', 'societa', 'welfare'] as const
+const selectedTab = ref('Tutti i temi')
+
+</script>
+
+<style scoped>
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-150px);
+}
+</style>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap');
+
+* {
+  font-family: Raleway;
+}
+</style>
